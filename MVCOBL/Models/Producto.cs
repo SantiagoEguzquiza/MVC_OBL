44fafe686f4feb85f7da6f85cb93f4124cae4a19
SantiagoEguzquiza <santieguzquiza@gmail.com>
2023-06-25 01:06:43 -0300

    FIRST COMMIT

---
+++ MVCOBL/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCOBL.Models
+{
+    public partial class Producto
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+    }
+}
