a1c38a5fd98d7b5b95425d253f74344a44c60ab6
lautaro <lautaroodeleonn@gmail.com>
2023-07-05 18:17:57 -0300

    CATALOGO HECHO

--- MVCOBL/Controllers/ProductosController.cs
+++ MVCOBL/Controllers/ProductosController.cs
@@ -10,167 +10,188 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace MVCOBL.Controllers
 {
-    public class ProductosController : Controller
-    {
-        private readonly MVCOBLContext _context;
+	public class ProductosController : Controller
+	{
+		private readonly MVCOBLContext _context;
 
-        public ProductosController(MVCOBLContext context)
-        {
-            _context = context;
-        }
+		public ProductosController(MVCOBLContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Productos
-        [Authorize]
-        public async Task<IActionResult> Index()
-        {
-            var mVCOBLContext = _context.Productos.Include(p => p.IdCategoriaNavigation);
-            return View(await mVCOBLContext.ToListAsync());
-        }
+		// GET: Productos
+		[Authorize]
+		public async Task<IActionResult> Index()
+		{
+			var mVCOBLContext = _context.Productos.Include(p => p.IdCategoriaNavigation);
+			return View(await mVCOBLContext.ToListAsync());
+			//var listarProducto = _context.Productos.ToList();
+			//return View(listarProducto);
+		}
+		public IActionResult Catalogo()
+		{
+			var listarProducto = _context.Productos.ToList();
+			return View(listarProducto);
+		}
 
-        // GET: Productos/Details/5
-        [Authorize]
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Productos == null)
-            {
-                return NotFound();
-            }
+		// GET: Productos/Details/5
+		[Authorize]
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null || _context.Productos == null)
+			{
+				return NotFound();
+			}
 
-            var producto = await _context.Productos
-                .Include(p => p.IdCategoriaNavigation)
-                .FirstOrDefaultAsync(m => m.IdProducto == id);
-            if (producto == null)
-            {
-                return NotFound();
-            }
+			var producto = await _context.Productos
+				.Include(p => p.IdCategoriaNavigation)
+				.FirstOrDefaultAsync(m => m.IdProducto == id);
+			if (producto == null)
+			{
+				return NotFound();
+			}
 
-            return View(producto);
-        }
+			return View(producto);
+		}
+		//public IActionResult Registro()
+		//{
+		//	return View();
+		//}
+		//public IActionResult Registrar(Producto prod)
+		//{
+		//	if (ModelState.IsValid)
+		//	{
+		//		_context.Add(prod);
+		//		_context.SaveChanges();
+		//		return RedirectToAction(nameof(Index));
+		//	}
+		//	return View(prod);
+		//}
 
 		// GET: Productos/Create
 		[Authorize(Roles = "Admin")]
 		public IActionResult Create()
-        {
-            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "IdCategoria");
-            return View();
-        }
+		{
+			ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "IdCategoria");
+			return View();
+		}
 
-        // POST: Productos/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+		// POST: Productos/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
 		[Authorize(Roles = "Admin")]
-		public async Task<IActionResult> Create([Bind("IdProducto,Codigo,Nombre,Descripcion,IdCategoria,FechaRegistro")] Producto producto)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(producto);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "IdCategoria", producto.IdCategoria);
-            return View(producto);
-        }
+		public async Task<IActionResult> Create([Bind("IdProducto,Codigo,Nombre,Descripcion,Precio,Stock,IdCategoria,ImagenUrl,FechaRegistro")] Producto producto)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(producto);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "IdCategoria", producto.IdCategoria);
+			return View(producto);
+		}
 
 		// GET: Productos/Edit/5
 		[Authorize(Roles = "Admin")]
 		public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Productos == null)
-            {
-                return NotFound();
-            }
+		{
+			if (id == null || _context.Productos == null)
+			{
+				return NotFound();
+			}
 
-            var producto = await _context.Productos.FindAsync(id);
-            if (producto == null)
-            {
-                return NotFound();
-            }
-            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "IdCategoria", producto.IdCategoria);
-            return View(producto);
-        }
+			var producto = await _context.Productos.FindAsync(id);
+			if (producto == null)
+			{
+				return NotFound();
+			}
+			ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "IdCategoria", producto.IdCategoria);
+			return View(producto);
+		}
 
-        // POST: Productos/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+		// POST: Productos/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
 		[Authorize(Roles = "Admin")]
-		public async Task<IActionResult> Edit(int id, [Bind("IdProducto,Codigo,Nombre,Descripcion,IdCategoria,FechaRegistro")] Producto producto)
-        {
-            if (id != producto.IdProducto)
-            {
-                return NotFound();
-            }
+		public async Task<IActionResult> Edit(int id, [Bind("IdProducto,Codigo,Nombre,Descripcion,Precio,Stock,IdCategoria,ImagenUrl,FechaRegistro")] Producto producto)
+		{
+			if (id != producto.IdProducto)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(producto);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductoExists(producto.IdProducto))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "IdCategoria", producto.IdCategoria);
-            return View(producto);
-        }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(producto);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!ProductoExists(producto.IdProducto))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "IdCategoria", producto.IdCategoria);
+			return View(producto);
+		}
 
 		// GET: Productos/Delete/5
 		[Authorize(Roles = "Admin")]
 		public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Productos == null)
-            {
-                return NotFound();
-            }
+		{
+			if (id == null || _context.Productos == null)
+			{
+				return NotFound();
+			}
 
-            var producto = await _context.Productos
-                .Include(p => p.IdCategoriaNavigation)
-                .FirstOrDefaultAsync(m => m.IdProducto == id);
-            if (producto == null)
-            {
-                return NotFound();
-            }
+			var producto = await _context.Productos
+				.Include(p => p.IdCategoriaNavigation)
+				.FirstOrDefaultAsync(m => m.IdProducto == id);
+			if (producto == null)
+			{
+				return NotFound();
+			}
 
-            return View(producto);
-        }
+			return View(producto);
+		}
 
-        // POST: Productos/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
+		// POST: Productos/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
 		[Authorize(Roles = "Admin")]
 		public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Productos == null)
-            {
-                return Problem("Entity set 'MVCOBLContext.Productos'  is null.");
-            }
-            var producto = await _context.Productos.FindAsync(id);
-            if (producto != null)
-            {
-                _context.Productos.Remove(producto);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+		{
+			if (_context.Productos == null)
+			{
+				return Problem("Entity set 'MVCOBLContext.Productos'  is null.");
+			}
+			var producto = await _context.Productos.FindAsync(id);
+			if (producto != null)
+			{
+				_context.Productos.Remove(producto);
+			}
 
-        private bool ProductoExists(int id)
-        {
-          return (_context.Productos?.Any(e => e.IdProducto == id)).GetValueOrDefault();
-        }
-    }
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool ProductoExists(int id)
+		{
+			return (_context.Productos?.Any(e => e.IdProducto == id)).GetValueOrDefault();
+		}
+	}
 }
