f19cbff422c2147989e78b54f3d09064e33236cb
lautaro <lautaroodeleonn@gmail.com>
2023-07-04 22:21:28 -0300

    ACT antes de hacer cambios

---
+++ MVC_OBL/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC_OBL.Models;
+
+public partial class Producto
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+}
